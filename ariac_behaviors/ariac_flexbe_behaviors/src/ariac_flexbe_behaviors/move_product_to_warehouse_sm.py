1380354aa24247f7a0b9565a334111fd1f7141aa
wesselkoolen <wessel.koolen@gmail.com>
2020-05-27 11:45:40 +0200

    commit wessel 27 5 20

--- ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/move_product_to_warehouse_sm.py
+++ ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/move_product_to_warehouse_sm.py
@@ -10,6 +10,8 @@
 from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
 from ariac_flexbe_states.start_assignment_state import StartAssignment
 from ariac_flexbe_states.set_conveyorbelt_power_state import SetConveyorbeltPowerState
+from ariac_flexbe_behaviors.move_home_belt_sm import move_home_beltSM
+from ariac_flexbe_states.detect_first_part_camera_ariac_state import DetectFirstPartCameraAriacState
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -33,6 +35,7 @@ class move_product_to_warehouseSM(Behavior):
 		# parameters of this behavior
 
 		# references to used behaviors
+		self.add_behavior(move_home_beltSM, 'move_home_belt')
 
 		# Additional initialization code can be added inside the following tags
 		# [MANUAL_INIT]
@@ -44,9 +47,22 @@ class move_product_to_warehouseSM(Behavior):
 
 
 	def create(self):
-		# x:631 y:281, x:242 y:360
+		# x:877 y:408, x:267 y:397
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
 		_state_machine.userdata.powerOn = 100
+		_state_machine.userdata.config_name_Home = 'Gantry_Home'
+		_state_machine.userdata.move_group = 'Gantry'
+		_state_machine.userdata.move_group_prefix = '/ariac/gantry'
+		_state_machine.userdata.action_topic = '/move_group'
+		_state_machine.userdata.robot_name = 'gantry'
+		_state_machine.userdata.joint_values = []
+		_state_machine.userdata.joint_names = []
+		_state_machine.userdata.powerOff = 0
+		_state_machine.userdata.ref_frame = ''
+		_state_machine.userdata.camera_topic = '/ariac/logical_camera_4'
+		_state_machine.userdata.camera_frame = 'logical_camera_4_frame'
+		_state_machine.userdata.part_type = ''
+		_state_machine.userdata.pose = []
 
 		# Additional creation code can be added inside the following tags
 		# [MANUAL_CREATE]
@@ -58,15 +74,35 @@ class move_product_to_warehouseSM(Behavior):
 			# x:30 y:40
 			OperatableStateMachine.add('StartAssignment',
 										StartAssignment(),
-										transitions={'continue': 'TurnOnConveyor'},
+										transitions={'continue': 'move_home_belt'},
 										autonomy={'continue': Autonomy.Off})
 
-			# x:169 y:40
+			# x:360 y:37
 			OperatableStateMachine.add('TurnOnConveyor',
+										SetConveyorbeltPowerState(),
+										transitions={'continue': 'CameraDetectPartPose', 'fail': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'fail': Autonomy.Off},
+										remapping={'power': 'powerOn'})
+
+			# x:178 y:38
+			OperatableStateMachine.add('move_home_belt',
+										self.use_behavior(move_home_beltSM, 'move_home_belt'),
+										transitions={'finished': 'TurnOnConveyor', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit})
+
+			# x:554 y:37
+			OperatableStateMachine.add('CameraDetectPartPose',
+										DetectFirstPartCameraAriacState(part_list=['piston_rod_part_red', 'gasket_part_blue'], time_out=0.5),
+										transitions={'continue': 'TurnOnConveyor_2', 'failed': 'failed', 'not_found': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off, 'not_found': Autonomy.Off},
+										remapping={'ref_frame': 'ref_frame', 'camera_topic': 'camera_topic', 'camera_frame': 'camera_frame', 'part': 'part_type', 'pose': 'pose'})
+
+			# x:769 y:38
+			OperatableStateMachine.add('TurnOnConveyor_2',
 										SetConveyorbeltPowerState(),
 										transitions={'continue': 'finished', 'fail': 'failed'},
 										autonomy={'continue': Autonomy.Off, 'fail': Autonomy.Off},
-										remapping={'power': 'powerOn'})
+										remapping={'power': 'powerOff'})
 
 
 		return _state_machine
---
+++ ariac_behaviors/ariac_flexbe_states/src/ariac_flexbe_states/gripper_control_state.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+import rospy
+import rostopic
+
+from flexbe_core import EventState, Logger
+from osrf_gear.srv import VacuumGripperControl
+from osrf_gear.msg import VacuumGripperState
+from std_msgs.msg import String
+
+# Authors: Wessel Koolen & Niels van Dongen
+
+class GripperControl(EventState):
+	'''
+	This state is used for enabling and disabling the gripper used on both robotarms
+
+	-- enable 	bool 		Enables the vacuum gripper True of False
+
+	#> arm_id	string		Arm identifier.
+
+	<= continue 			Given time has passed.
+	<= failed 				Example for a failure outcome.
+	<= invalid_id			arm_id invalid
+
+	'''
+
+	def __init__(self, enable):
+		# Declare outcomes, input_keys, and output_keys by calling the super constructor with the corresponding arguments.
+		super(GripperControl, self).__init__(input_keys = ['arm_id'], outcomes = ['continue', 'failed', 'invalid_id'])
+
+		# Store state parameter for later use.
+
+		self._enable = enable
+		# The constructor is called when building the state machine, not when actually starting the behavior.
+		# Thus, we cannot save the starting time now and will do so later.
+
+
+	def execute(self, userdata):
+		if userdata.arm_id == 'Left_Arm':
+			gripper_service = '/ariac/Left_Arm/gripper/control'
+
+		elif userdata.arm_id == 'Right_Arm':
+			gripper_service = '/ariac/Right_Arm/gripper/control'
+
+		else:
+			return 'invalid_id'
+
+		rospy.loginfo("Waiting for service")
+		rospy.wait_for_service(gripper_service)
+		try:
+			gripper_control = rospy.ServiceProxy(gripper_service, VacuumGripperControl)
+			request = VacuumGripperControlRequest()
+			request.enable = self._enable
+
+			service_response = gripper_control(request)
+
+			if service_response.success == True:
+				if self._enable == True:
+					if userdata.arm_id == 'Left_Arm':
+						status = rospy.wait_for_message('/ariac/Left_Arm/gripper/state', VacuumGripperState)
+						if status.attached == True:
+							return 'continue'
+					elif userdata.arm_id == 'Right_Arm':
+						status = rospy.wait_for_message('/ariac/Right_Arm/gripper/state', VacuumGripperState)
+						if status.attached == True:
+							return 'continue'
+					else:
+						return 'failed'
+				else:
+					return 'continue'
+			else:
+				return 'failed'
+
+		except rospy.ServiceException, e:
+			rospy.loginfo("Service call failed: %s"%e)
+			return 'failed'
+
+		# This method is called periodically while the state is active.
+		# Main purpose is to check state conditions and trigger a corresponding outcome.
+		# If no outcome is returned, the state will stay active.
+		pass # Nothing to do in this example.
+
+
+	def on_exit(self, userdata):
+		# This method is called when an outcome is returned and another state gets active.
+		# It can be used to stop possibly running processes started by on_enter.
+
+		pass # Nothing to do in this example.
+
+
+	def on_start(self):
+		# This method is called when the behavior is started.
+		# If possible, it is generally better to initialize used resources in the constructor
+		# because if anything failed, the behavior would not even be started.
+
+		# In this example, we use this event to set the correct start time.
+		pass #nothing in this example
+
+
+	def on_stop(self):
+		# This method is called whenever the behavior stops execution, also if it is cancelled.
+		# Use this event to clean up things like claimed resources.
+
+		pass # Nothing to do in this example.
