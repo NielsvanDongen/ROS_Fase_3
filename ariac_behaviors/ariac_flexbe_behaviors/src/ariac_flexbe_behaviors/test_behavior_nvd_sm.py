b646bc1d9c40e1e1552a7484626a63beb9ab19cc
NielsvanDongen <n.vandongen4@student.avans.nl>
2020-05-28 11:51:49 +0200

    Home behavior toegegoegd

--- ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/test_behavior_nvd_sm.py
+++ ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/test_behavior_nvd_sm.py
@@ -11,6 +11,7 @@ from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyC
 from ariac_flexbe_states.find_correct_bin import FindCorrectBin
 from flexbe_states.wait_state import WaitState
 from ariac_flexbe_states.srdf_state_to_moveit_ariac_state import SrdfStateToMoveitAriac
+from ariac_flexbe_behaviors.move_home_belt_sm import move_home_beltSM
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -34,6 +35,7 @@ class Test_behavior_nvdSM(Behavior):
 		# parameters of this behavior
 
 		# references to used behaviors
+		self.add_behavior(move_home_beltSM, 'move_home_belt')
 
 		# Additional initialization code can be added inside the following tags
 		# [MANUAL_INIT]
@@ -64,12 +66,11 @@ class Test_behavior_nvdSM(Behavior):
 
 
 		with _state_machine:
-			# x:559 y:28
-			OperatableStateMachine.add('Find_product_location',
-										FindCorrectBin(time_out=0.5),
-										transitions={'continue': 'move_gantry_bin_gr1', 'failed': 'failed'},
-										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
-										remapping={'part_type': 'part_type', 'bin': 'bin', 'camera_topic': 'camera_topic', 'camera_frame': 'camera_frame', 'ref_frame': 'ref_frame'})
+			# x:230 y:22
+			OperatableStateMachine.add('move_home_belt',
+										self.use_behavior(move_home_beltSM, 'move_home_belt'),
+										transitions={'finished': 'Find_product_location', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit})
 
 			# x:810 y:40
 			OperatableStateMachine.add('Waitretry',
@@ -83,6 +84,13 @@ class Test_behavior_nvdSM(Behavior):
 										transitions={'reached': 'finished', 'planning_failed': 'Waitretry', 'control_failed': 'Waitretry', 'param_error': 'Waitretry'},
 										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
 										remapping={'config_name': 'bin', 'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:559 y:28
+			OperatableStateMachine.add('Find_product_location',
+										FindCorrectBin(time_out=0.5),
+										transitions={'continue': 'move_gantry_bin_gr1', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'part_type': 'part_type', 'bin': 'bin', 'camera_topic': 'camera_topic', 'camera_frame': 'camera_frame', 'ref_frame': 'ref_frame'})
 
 
 		return _state_machine
