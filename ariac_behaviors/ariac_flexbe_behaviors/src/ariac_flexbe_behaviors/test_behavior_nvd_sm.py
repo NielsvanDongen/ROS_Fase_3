96aea630ed373741812fda4a79d14664d0b06906
NielsvanDongen <n.vandongen4@student.avans.nl>
2020-05-29 13:42:02 +0200

    kut zooi

--- ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/test_behavior_nvd_sm.py
+++ ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/test_behavior_nvd_sm.py
@@ -8,10 +8,16 @@
 ###########################################################
 
 from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
-from ariac_flexbe_states.find_correct_bin import FindCorrectBin
+from ariac_flexbe_behaviors.move_home_belt_sm import move_home_beltSM
 from flexbe_states.wait_state import WaitState
 from ariac_flexbe_states.srdf_state_to_moveit_ariac_state import SrdfStateToMoveitAriac
-from ariac_flexbe_behaviors.move_home_belt_sm import move_home_beltSM
+from ariac_flexbe_states.find_correct_bin import FindCorrectBin
+from ariac_flexbe_states.compute_grasp_ariac_state import ComputeGraspAriacState
+from ariac_flexbe_states.detect_part_camera_ariac_state import DetectPartCameraAriacState
+from ariac_flexbe_states.decide_offset_product import DecideOffsetProduct
+from ariac_flexbe_states.gripper_control_state import GripperControl
+from ariac_flexbe_behaviors.move_home_robot_r_sm import move_home_robot_rSM
+from ariac_flexbe_states.moveit_to_joints_dyn_ariac_state import MoveitToJointsDynAriacState
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -36,6 +42,7 @@ class Test_behavior_nvdSM(Behavior):
 
 		# references to used behaviors
 		self.add_behavior(move_home_beltSM, 'move_home_belt')
+		self.add_behavior(move_home_robot_rSM, 'move_home_robot_r')
 
 		# Additional initialization code can be added inside the following tags
 		# [MANUAL_INIT]
@@ -47,7 +54,7 @@ class Test_behavior_nvdSM(Behavior):
 
 
 	def create(self):
-		# x:631 y:281, x:242 y:360
+		# x:27 y:469, x:242 y:360
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
 		_state_machine.userdata.powerOn = 100
 		_state_machine.userdata.config_name = ''
@@ -58,6 +65,14 @@ class Test_behavior_nvdSM(Behavior):
 		_state_machine.userdata.part_type = 'pulley_part_red'
 		_state_machine.userdata.joint_values = []
 		_state_machine.userdata.joint_names = []
+		_state_machine.userdata.ref_frame = 'world'
+		_state_machine.userdata.camera_topic = ''
+		_state_machine.userdata.camera_frame = ''
+		_state_machine.userdata.tool_link_r = 'right_ee_link'
+		_state_machine.userdata.pose = ''
+		_state_machine.userdata.rotation = 0
+		_state_machine.userdata.move_groupR = 'Right_Arm'
+		_state_machine.userdata.arm_id_r = 'Right_Arm'
 
 		# Additional creation code can be added inside the following tags
 		# [MANUAL_CREATE]
@@ -81,7 +96,7 @@ class Test_behavior_nvdSM(Behavior):
 			# x:701 y:151
 			OperatableStateMachine.add('move_gantry_bin_gr1',
 										SrdfStateToMoveitAriac(),
-										transitions={'reached': 'finished', 'planning_failed': 'Waitretry', 'control_failed': 'Waitretry', 'param_error': 'Waitretry'},
+										transitions={'reached': 'find part', 'planning_failed': 'Waitretry', 'control_failed': 'Waitretry', 'param_error': 'Waitretry'},
 										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
 										remapping={'config_name': 'bin', 'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
 
@@ -91,6 +106,47 @@ class Test_behavior_nvdSM(Behavior):
 										transitions={'continue': 'move_gantry_bin_gr1', 'failed': 'failed'},
 										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
 										remapping={'part_type': 'part_type', 'bin': 'bin', 'camera_topic': 'camera_topic', 'camera_frame': 'camera_frame', 'ref_frame': 'ref_frame'})
+
+			# x:694 y:497
+			OperatableStateMachine.add('compute_pick _r',
+										ComputeGraspAriacState(joint_names=['right_shoulder_pan_joint', 'right_shoulder_lift_joint', 'right_elbow_joint', 'right_wrist_1_joint', 'right_wrist_2_joint', 'right_wrist_3_joint']),
+										transitions={'continue': 'Move to pick', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'move_group': 'move_groupR', 'move_group_prefix': 'move_group_prefix', 'tool_link': 'tool_link_r', 'pose': 'pose', 'offset': 'part_offset', 'rotation': 'rotation', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:713 y:240
+			OperatableStateMachine.add('find part',
+										DetectPartCameraAriacState(time_out=0.5),
+										transitions={'continue': 'Offset bepalen', 'failed': 'failed', 'not_found': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off, 'not_found': Autonomy.Off},
+										remapping={'ref_frame': 'ref_frame', 'camera_topic': 'camera_topic', 'camera_frame': 'camera_frame', 'part': 'part_type', 'pose': 'pose'})
+
+			# x:710 y:394
+			OperatableStateMachine.add('Offset bepalen',
+										DecideOffsetProduct(target_time=0.5),
+										transitions={'succes': 'compute_pick _r', 'unknown_id': 'failed'},
+										autonomy={'succes': Autonomy.Off, 'unknown_id': Autonomy.Off},
+										remapping={'part_type': 'part_type', 'part_offset': 'part_offset'})
+
+			# x:433 y:563
+			OperatableStateMachine.add('Gripper_enable',
+										GripperControl(enable=True),
+										transitions={'continue': 'move_home_robot_r', 'failed': 'Move to pick', 'invalid_id': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off, 'invalid_id': Autonomy.Off},
+										remapping={'arm_id': 'arm_id_r'})
+
+			# x:234 y:591
+			OperatableStateMachine.add('move_home_robot_r',
+										self.use_behavior(move_home_robot_rSM, 'move_home_robot_r'),
+										transitions={'finished': 'finished', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit})
+
+			# x:619 y:590
+			OperatableStateMachine.add('Move to pick',
+										MoveitToJointsDynAriacState(),
+										transitions={'reached': 'Gripper_enable', 'planning_failed': 'failed', 'control_failed': 'Gripper_enable'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
+										remapping={'move_group_prefix': 'move_group_prefix', 'move_group': 'move_groupR', 'action_topic': 'action_topic', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
 
 
 		return _state_machine
