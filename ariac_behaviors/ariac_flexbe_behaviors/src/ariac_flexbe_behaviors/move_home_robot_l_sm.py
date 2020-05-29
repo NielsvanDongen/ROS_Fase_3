22f39f44913ac9ec9cf56dd6824f959ac266ac1f
NielsvanDongen <n.vandongen4@student.avans.nl>
2020-05-29 11:19:44 +0200

    kutzooi

---
+++ ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/move_home_robot_l_sm.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###########################################################
+#               WARNING: Generated code!                  #
+#              **************************                 #
+# Manual changes may get lost if file is generated again. #
+# Only code inside the [MANUAL] tags will be kept.        #
+###########################################################
+
+from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
+from ariac_flexbe_states.srdf_state_to_moveit_ariac_state import SrdfStateToMoveitAriac
+# Additional imports can be added inside the following tags
+# [MANUAL_IMPORT]
+
+# [/MANUAL_IMPORT]
+
+
+'''
+Created on Wed May 27 2020
+@author: Niels van Dongen
+'''
+class move_home_robot_lSM(Behavior):
+	'''
+	Moves left robot to home position.
+	'''
+
+
+	def __init__(self):
+		super(move_home_robot_lSM, self).__init__()
+		self.name = 'move_home_robot_l'
+
+		# parameters of this behavior
+
+		# references to used behaviors
+
+		# Additional initialization code can be added inside the following tags
+		# [MANUAL_INIT]
+		
+		# [/MANUAL_INIT]
+
+		# Behavior comments:
+
+
+
+	def create(self):
+		# x:628 y:51, x:130 y:406
+		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
+		_state_machine.userdata.config_name_left = 'Left_Home'
+		_state_machine.userdata.move_group_g = 'Gantry'
+		_state_machine.userdata.move_group_prefix_g = '/ariac/gantry'
+		_state_machine.userdata.action_topic = '/move_group'
+		_state_machine.userdata.robot_name = 'gantry'
+		_state_machine.userdata.joint_values = []
+		_state_machine.userdata.joint_names = []
+		_state_machine.userdata.move_group_l = 'Left_Arm'
+
+		# Additional creation code can be added inside the following tags
+		# [MANUAL_CREATE]
+		
+		# [/MANUAL_CREATE]
+
+
+		with _state_machine:
+			# x:30 y:40
+			OperatableStateMachine.add('MoveLeftArm',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'finished', 'planning_failed': 'failed', 'control_failed': 'failed', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_left', 'move_group': 'move_group_l', 'move_group_prefix': 'move_group_prefix_g', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+
+		return _state_machine
+
+
+	# Private functions can be added inside the following tags
+	# [MANUAL_FUNC]
+	
+	# [/MANUAL_FUNC]
