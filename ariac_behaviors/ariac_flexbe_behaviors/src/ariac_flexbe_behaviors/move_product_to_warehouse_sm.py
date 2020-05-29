feed3a3909478269cbacc59a4df33a25b4268f5c
wesselkoolen <wessel.koolen@gmail.com>
2020-05-29 11:10:26 +0200

    wessel 29-05-20

--- ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/move_product_to_warehouse_sm.py
+++ ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/move_product_to_warehouse_sm.py
@@ -14,7 +14,6 @@ from ariac_flexbe_behaviors.move_home_belt_sm import move_home_beltSM
 from ariac_flexbe_states.compute_grasp_ariac_state import ComputeGraspAriacState
 from flexbe_states.wait_state import WaitState
 from ariac_flexbe_states.moveit_to_joints_dyn_ariac_state import MoveitToJointsDynAriacState
-from ariac_flexbe_states.decide_offset_product import DecideOffsetProduct
 from ariac_flexbe_states.detect_first_part_camera_ariac_state import DetectFirstPartCameraAriacState
 from ariac_flexbe_states.srdf_state_to_moveit_ariac_state import SrdfStateToMoveitAriac
 # Additional imports can be added inside the following tags
@@ -55,26 +54,27 @@ class move_product_to_warehouseSM(Behavior):
 		# x:1227 y:622, x:267 y:397
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
 		_state_machine.userdata.powerOn = 100
-		_state_machine.userdata.move_group = 'Right_Arm'
+		_state_machine.userdata.move_group_g = 'Gantry'
 		_state_machine.userdata.move_group_prefix = '/ariac/gantry'
 		_state_machine.userdata.action_topic = '/move_group'
-		_state_machine.userdata.robot_name = 'gantry'
+		_state_machine.userdata.robot_name = ''
 		_state_machine.userdata.joint_values = []
 		_state_machine.userdata.joint_names = []
 		_state_machine.userdata.powerOff = 0
 		_state_machine.userdata.ref_frame = 'world'
 		_state_machine.userdata.camera_topic = '/ariac/logical_camera_6'
 		_state_machine.userdata.camera_frame = 'logical_camera_6_frame'
-		_state_machine.userdata.part_type = 'piston_rod_part_red'
 		_state_machine.userdata.pose = []
 		_state_machine.userdata.camera_topic_7 = '/ariac/logical_camera_7'
 		_state_machine.userdata.camera_frame_7 = 'logical_camera_7_frame'
-		_state_machine.userdata.part_offset = []
+		_state_machine.userdata.part_offset = 0
 		_state_machine.userdata.rotation = 0
 		_state_machine.userdata.tool_link = 'right_ee_link'
 		_state_machine.userdata.config_name_pregrasp = 'beltPreGrasp'
-		_state_machine.userdata.part_list = ['piston_rod_part_red', 'piston_rod_part_red_1', 'piston_rod_part_red_2', 'gasket_part_blue_0', 'gasket_part_blue_1', 'gasket_part_blue_2']
-		_state_machine.userdata.move_group = 'Gantry'
+		_state_machine.userdata.part_type = ''
+		_state_machine.userdata.move_group = 'Right_Arm'
+		_state_machine.userdata.joint_names_r = ['right_shoulder_pan_joint', 'right_shoulder_lift_joint', 'right_elbow_joint', 'right_wrist_1_joint', 'right_wrist_2_joint', 'right_wrist_3_joint']
+		_state_machine.userdata.robot_name_g = ''
 
 		# Additional creation code can be added inside the following tags
 		# [MANUAL_CREATE]
@@ -125,21 +125,14 @@ class move_product_to_warehouseSM(Behavior):
 			# x:1120 y:284
 			OperatableStateMachine.add('PickProduct',
 										MoveitToJointsDynAriacState(),
-										transitions={'reached': 'finished', 'planning_failed': 'failed', 'control_failed': 'failed'},
+										transitions={'reached': 'MoveUp', 'planning_failed': 'failed', 'control_failed': 'failed'},
 										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
 										remapping={'move_group_prefix': 'move_group_prefix', 'move_group': 'move_group', 'action_topic': 'action_topic', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
-
-			# x:1154 y:127
-			OperatableStateMachine.add('DecideOffset',
-										DecideOffsetProduct(target_time=0.5),
-										transitions={'succes': 'ComputeGrasp', 'unknown_id': 'DetectFirstPartBelt_2'},
-										autonomy={'succes': Autonomy.Off, 'unknown_id': Autonomy.Off},
-										remapping={'part_type': 'part_type', 'part_offset': 'part_offset'})
 
 			# x:561 y:38
 			OperatableStateMachine.add('DetectFirstPartBelt',
 										DetectFirstPartCameraAriacState(part_list=['piston_rod_part_red', 'gasket_part_blue_0', 'gasket_part_blue_1', 'gasket_part_blue_2'], time_out=0.5),
-										transitions={'continue': 'TurnOffConveyor', 'failed': 'failed', 'not_found': 'WaitRetry'},
+										transitions={'continue': 'TurnOffConveyor', 'failed': 'WaitRetry', 'not_found': 'WaitRetry'},
 										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off, 'not_found': Autonomy.Off},
 										remapping={'ref_frame': 'ref_frame', 'camera_topic': 'camera_topic', 'camera_frame': 'camera_frame', 'part': 'part_type', 'pose': 'pose'})
 
@@ -153,9 +146,16 @@ class move_product_to_warehouseSM(Behavior):
 			# x:1164 y:36
 			OperatableStateMachine.add('MovePreGraspBelt',
 										SrdfStateToMoveitAriac(),
-										transitions={'reached': 'DecideOffset', 'planning_failed': 'failed', 'control_failed': 'DecideOffset', 'param_error': 'failed'},
+										transitions={'reached': 'ComputeGrasp', 'planning_failed': 'failed', 'control_failed': 'failed', 'param_error': 'failed'},
 										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
-										remapping={'config_name': 'config_name_pregrasp', 'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+										remapping={'config_name': 'config_name_pregrasp', 'move_group': 'move_group_g', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name_g', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:1147 y:439
+			OperatableStateMachine.add('MoveUp',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'finished', 'planning_failed': 'failed', 'control_failed': 'failed', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_pregrasp', 'move_group': 'move_group_g', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name_g', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
 
 
 		return _state_machine
