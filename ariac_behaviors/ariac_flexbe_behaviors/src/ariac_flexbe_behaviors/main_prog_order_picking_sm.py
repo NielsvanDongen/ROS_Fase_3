7ff8ef1fd621a4b6e274cbe27aea3ee704d605f7
NielsvanDongen <n.vandongen4@student.avans.nl>
2020-06-03 11:31:01 +0200

    Hoofd programma deel NvD

---
+++ ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/main_prog_order_picking_sm.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###########################################################
+#               WARNING: Generated code!                  #
+#              **************************                 #
+# Manual changes may get lost if file is generated again. #
+# Only code inside the [MANUAL] tags will be kept.        #
+###########################################################
+
+from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
+from ariac_flexbe_states.start_assignment_state import StartAssignment
+from ariac_logistics_flexbe_states.get_order_state import GetOrderState
+from ariac_logistics_flexbe_states.get_products_from_shipment_state import GetProductsFromShipmentState
+from ariac_logistics_flexbe_states.get_part_from_products_state import GetPartFromProductsState
+from ariac_flexbe_behaviors.move_home_belt_sm import move_home_beltSM
+from ariac_flexbe_behaviors.order_picking_sm import Order_pickingSM
+from ariac_support_flexbe_states.add_numeric_state import AddNumericState
+from ariac_support_flexbe_states.equal_state import EqualState
+# Additional imports can be added inside the following tags
+# [MANUAL_IMPORT]
+
+# [/MANUAL_IMPORT]
+
+
+'''
+Created on Thu May 27 2020
+@author: Niels van Dongen
+'''
+class Main_prog_order_pickingSM(Behavior):
+	'''
+	Main program for order picking
+	'''
+
+
+	def __init__(self):
+		super(Main_prog_order_pickingSM, self).__init__()
+		self.name = 'Main_prog_order_picking'
+
+		# parameters of this behavior
+
+		# references to used behaviors
+		self.add_behavior(move_home_beltSM, 'move_home_belt')
+		self.add_behavior(Order_pickingSM, 'Order_picking')
+
+		# Additional initialization code can be added inside the following tags
+		# [MANUAL_INIT]
+		
+		# [/MANUAL_INIT]
+
+		# Behavior comments:
+
+
+
+	def create(self):
+		# x:27 y:469, x:242 y:360
+		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
+		_state_machine.userdata.powerOn = 100
+		_state_machine.userdata.config_name = ''
+		_state_machine.userdata.move_group_g = 'Gantry'
+		_state_machine.userdata.move_group_prefix = '/ariac/gantry'
+		_state_machine.userdata.action_topic = '/move_group'
+		_state_machine.userdata.robot_name = 'gantry'
+		_state_machine.userdata.joint_values = []
+		_state_machine.userdata.joint_names = []
+		_state_machine.userdata.ref_frame = 'world'
+		_state_machine.userdata.camera_topic = ''
+		_state_machine.userdata.camera_frame = ''
+		_state_machine.userdata.tool_link = ''
+		_state_machine.userdata.pose = ''
+		_state_machine.userdata.rotation = 0
+		_state_machine.userdata.move_group = ''
+		_state_machine.userdata.arm_id = ''
+		_state_machine.userdata.index = 0
+		_state_machine.userdata.part_type = ''
+		_state_machine.userdata.one = 1
+		_state_machine.userdata.product_itt = 0
+
+		# Additional creation code can be added inside the following tags
+		# [MANUAL_CREATE]
+		
+		# [/MANUAL_CREATE]
+
+
+		with _state_machine:
+			# x:99 y:29
+			OperatableStateMachine.add('Start_assignment',
+										StartAssignment(),
+										transitions={'continue': 'move_home_belt'},
+										autonomy={'continue': Autonomy.Off})
+
+			# x:431 y:28
+			OperatableStateMachine.add('get_orders',
+										GetOrderState(),
+										transitions={'continue': 'Get shipments'},
+										autonomy={'continue': Autonomy.Off},
+										remapping={'order_id': 'order_id', 'shipments': 'shipments', 'number_of_shipments': 'number_of_shipments'})
+
+			# x:615 y:25
+			OperatableStateMachine.add('Get shipments',
+										GetProductsFromShipmentState(),
+										transitions={'continue': 'g', 'invalid_index': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'invalid_index': Autonomy.Off},
+										remapping={'shipments': 'shipments', 'index': 'index', 'shipment_type': 'shipment_type', 'agv_id': 'agv_id', 'products': 'products', 'number_of_products': 'number_of_products'})
+
+			# x:833 y:35
+			OperatableStateMachine.add('g',
+										GetPartFromProductsState(),
+										transitions={'continue': 'Order_picking', 'invalid_index': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'invalid_index': Autonomy.Off},
+										remapping={'products': 'products', 'index': 'index', 'type': 'part_type', 'pose': 'pose'})
+
+			# x:261 y:28
+			OperatableStateMachine.add('move_home_belt',
+										self.use_behavior(move_home_beltSM, 'move_home_belt'),
+										transitions={'finished': 'get_orders', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit})
+
+			# x:1017 y:44
+			OperatableStateMachine.add('Order_picking',
+										self.use_behavior(Order_pickingSM, 'Order_picking'),
+										transitions={'finished': 'add_itterator order', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit})
+
+			# x:1038 y:147
+			OperatableStateMachine.add('add_itterator order',
+										AddNumericState(),
+										transitions={'done': 'Check_product_amount'},
+										autonomy={'done': Autonomy.Off},
+										remapping={'value_a': 'one', 'value_b': 'product_itt', 'result': 'product_itt'})
+
+			# x:1039 y:251
+			OperatableStateMachine.add('Check_product_amount',
+										EqualState(),
+										transitions={'true': 'move_home_belt', 'false': 'get_orders'},
+										autonomy={'true': Autonomy.Off, 'false': Autonomy.Off},
+										remapping={'value_a': 'number_of_products', 'value_b': 'product_itt'})
+
+
+		return _state_machine
+
+
+	# Private functions can be added inside the following tags
+	# [MANUAL_FUNC]
+	
+	# [/MANUAL_FUNC]
---
+++ ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/order_picking_sm.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###########################################################
+#               WARNING: Generated code!                  #
+#              **************************                 #
+# Manual changes may get lost if file is generated again. #
+# Only code inside the [MANUAL] tags will be kept.        #
+###########################################################
+
+from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
+from ariac_flexbe_states.find_correct_bin import FindCorrectBin
+from ariac_flexbe_states.srdf_state_to_moveit_ariac_state import SrdfStateToMoveitAriac
+from ariac_flexbe_states.compute_grasp_ariac_state import ComputeGraspAriacState
+from ariac_flexbe_states.detect_part_camera_ariac_state import DetectPartCameraAriacState
+from ariac_flexbe_states.decide_offset_product import DecideOffsetProduct
+from ariac_flexbe_states.gripper_control_state import GripperControl
+from ariac_flexbe_states.moveit_to_joints_dyn_ariac_state import MoveitToJointsDynAriacState
+from ariac_flexbe_states.gripper_active_check import GripperActiveCheck
+from flexbe_states.wait_state import WaitState
+# Additional imports can be added inside the following tags
+# [MANUAL_IMPORT]
+
+# [/MANUAL_IMPORT]
+
+
+'''
+Created on Thu May 27 2020
+@author: Niels van Dongen
+'''
+class Order_pickingSM(Behavior):
+	'''
+	behavior for picking products from bin 
+	'''
+
+
+	def __init__(self):
+		super(Order_pickingSM, self).__init__()
+		self.name = 'Order_picking'
+
+		# parameters of this behavior
+
+		# references to used behaviors
+
+		# Additional initialization code can be added inside the following tags
+		# [MANUAL_INIT]
+		
+		# [/MANUAL_INIT]
+
+		# Behavior comments:
+
+
+
+	def create(self):
+		# x:27 y:469, x:647 y:299
+		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
+		_state_machine.userdata.powerOn = 100
+		_state_machine.userdata.config_name = ''
+		_state_machine.userdata.move_group_g = 'Gantry'
+		_state_machine.userdata.move_group_prefix = '/ariac/gantry'
+		_state_machine.userdata.action_topic = '/move_group'
+		_state_machine.userdata.robot_name = 'gantry'
+		_state_machine.userdata.part_type = 'pulley_part_red'
+		_state_machine.userdata.joint_values = []
+		_state_machine.userdata.joint_names = []
+		_state_machine.userdata.ref_frame = 'world'
+		_state_machine.userdata.camera_topic = ''
+		_state_machine.userdata.camera_frame = ''
+		_state_machine.userdata.tool_link = ''
+		_state_machine.userdata.pose = ''
+		_state_machine.userdata.rotation = 0
+		_state_machine.userdata.move_group = ''
+		_state_machine.userdata.arm_id = ''
+		_state_machine.userdata.config_name_r = 'Right_Home'
+		_state_machine.userdata.config_name_l = 'Left_home'
+
+		# Additional creation code can be added inside the following tags
+		# [MANUAL_CREATE]
+		
+		# [/MANUAL_CREATE]
+
+
+		with _state_machine:
+			# x:152 y:81
+			OperatableStateMachine.add('Find_product_location',
+										FindCorrectBin(time_out=0.5),
+										transitions={'continue': 'move_gantry_bin_gr1', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'part_type': 'part_type', 'bin': 'bin', 'camera_topic': 'camera_topic', 'camera_frame': 'camera_frame', 'ref_frame': 'ref_frame'})
+
+			# x:338 y:86
+			OperatableStateMachine.add('move_gantry_bin_gr1',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'find part', 'planning_failed': 'Waitretry', 'control_failed': 'Waitretry', 'param_error': 'Waitretry'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'bin', 'move_group': 'move_group_g', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:945 y:160
+			OperatableStateMachine.add('compute_pick _r',
+										ComputeGraspAriacState(joint_names=['right_shoulder_pan_joint', 'right_shoulder_lift_joint', 'right_elbow_joint', 'right_wrist_1_joint', 'right_wrist_2_joint', 'right_wrist_3_joint']),
+										transitions={'continue': 'Move to pick_r', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'tool_link': 'tool_link', 'pose': 'pose', 'offset': 'part_offset', 'rotation': 'rotation', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:544 y:68
+			OperatableStateMachine.add('find part',
+										DetectPartCameraAriacState(time_out=0.5),
+										transitions={'continue': 'Offset bepalen', 'failed': 'failed', 'not_found': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off, 'not_found': Autonomy.Off},
+										remapping={'ref_frame': 'ref_frame', 'camera_topic': 'camera_topic', 'camera_frame': 'camera_frame', 'part': 'part_type', 'pose': 'pose'})
+
+			# x:760 y:17
+			OperatableStateMachine.add('Offset bepalen',
+										DecideOffsetProduct(target_time=0.5),
+										transitions={'succes': 'check_free_gripper', 'unknown_id': 'failed'},
+										autonomy={'succes': Autonomy.Off, 'unknown_id': Autonomy.Off},
+										remapping={'part_type': 'part_type', 'part_offset': 'part_offset'})
+
+			# x:959 y:326
+			OperatableStateMachine.add('Gripper_enable_r',
+										GripperControl(enable=True),
+										transitions={'continue': 'Move_pre_grasp', 'failed': 'Move to pick_r', 'invalid_id': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off, 'invalid_id': Autonomy.Off},
+										remapping={'arm_id': 'arm_id'})
+
+			# x:949 y:244
+			OperatableStateMachine.add('Move to pick_r',
+										MoveitToJointsDynAriacState(),
+										transitions={'reached': 'Gripper_enable_r', 'planning_failed': 'failed', 'control_failed': 'Gripper_enable_r'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
+										remapping={'move_group_prefix': 'move_group_prefix', 'move_group': 'move_group', 'action_topic': 'action_topic', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:921 y:17
+			OperatableStateMachine.add('check_free_gripper',
+										GripperActiveCheck(),
+										transitions={'Left': 'compute_pick _l', 'Right': 'compute_pick _r', 'failed': 'failed', 'Full': 'finished'},
+										autonomy={'Left': Autonomy.Off, 'Right': Autonomy.Off, 'failed': Autonomy.Off, 'Full': Autonomy.Off},
+										remapping={'arm_id': 'arm_id', 'tool_link': 'tool_link', 'move_group': 'move_group'})
+
+			# x:1292 y:51
+			OperatableStateMachine.add('compute_pick _l',
+										ComputeGraspAriacState(joint_names=['left_shoulder_pan_joint', 'left_shoulder_lift_joint', 'left_elbow_joint', 'left_wrist_1_joint', 'left_wrist_2_joint', 'left_wrist_3_joint']),
+										transitions={'continue': 'Move to pick_l', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'tool_link': 'tool_link', 'pose': 'pose', 'offset': 'part_offset', 'rotation': 'rotation', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:873 y:409
+			OperatableStateMachine.add('Move_pre_grasp',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'finished', 'planning_failed': 'failed', 'control_failed': 'failed', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_r', 'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:1273 y:273
+			OperatableStateMachine.add('Gripper_enable_l',
+										GripperControl(enable=True),
+										transitions={'continue': 'Move_pre_grasp_2', 'failed': 'Move to pick_l', 'invalid_id': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off, 'invalid_id': Autonomy.Off},
+										remapping={'arm_id': 'arm_id'})
+
+			# x:1239 y:164
+			OperatableStateMachine.add('Move to pick_l',
+										MoveitToJointsDynAriacState(),
+										transitions={'reached': 'Gripper_enable_l', 'planning_failed': 'failed', 'control_failed': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
+										remapping={'move_group_prefix': 'move_group_prefix', 'move_group': 'move_group', 'action_topic': 'action_topic', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:1269 y:384
+			OperatableStateMachine.add('Move_pre_grasp_2',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'finished', 'planning_failed': 'failed', 'control_failed': 'failed', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_l', 'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:436 y:3
+			OperatableStateMachine.add('Waitretry',
+										WaitState(wait_time=2),
+										transitions={'done': 'move_gantry_bin_gr1'},
+										autonomy={'done': Autonomy.Off})
+
+
+		return _state_machine
+
+
+	# Private functions can be added inside the following tags
+	# [MANUAL_FUNC]
+	
+	# [/MANUAL_FUNC]
